9eaf57de337b2a30bd425a05361db90e84c20147
flintheavengani <flintheavengani.scc@gmail.com>
2025-09-05 05:00:50 +0000

    done

--- Seatwork.java
+++ Seatwork.java
@@ -1,4 +1,4 @@
-public class Seatwork5 {
+public class Seatwork {
         public static void main(String[] args) {
                 System.out.println("Seatwork5");
                         int countdown = 3;
