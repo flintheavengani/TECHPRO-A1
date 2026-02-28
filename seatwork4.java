f51ccfbf2d2cf7ccafcf245a2b42caf6970f207c
flintheavengani <flintheavengani.scc@gmail.com>
2026-02-28 05:15:39 +0000

    commit

--- seatwork4.java
+++ seatwork4.java
@@ -1,1 +1,21 @@
+public class PaymentProcessor {
 
+        public static void processPayment(double amount, String currency) {
+                System.out.println("Processing payment of " + amount + " " + currency);
+                    }
+
+                        public void processPayment(double amount, String currency, String paymentMethod) {
+                                System.out.println("Processing payment of " + amount + " " + currency + " via " + paymentMethod);
+                                    }
+
+                                        public static void main(String[] args) {
+
+                                                // static method po itetch
+                                                        processPayment(500.00, "PHP");
+
+                                                                // non-static method po itetchh
+                                                                        PaymentProcessor obj = new PaymentProcessor();
+                                                                                obj.processPayment(1000.00, "USD", "Credit Card");
+                                                                                    }
+                                                                                    }
+}
